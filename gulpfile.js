8d118c832df85d746ef08836dfac961016ff6f75
SESO\cth <cth@seso.at>
2017-04-24 17:15:28 +0200

    building gruntfile

---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+// --------------------------------------------------------------------
+// define all required plugins
+// --------------------------------------------------------------------
+
+var   gulp         = require('gulp'),
+      sass         = require('gulp-sass'),
+      autoprefixer = require('gulp-autoprefixer'),
+      csso         = require('gulp-csso'),
+      uglify       = require('gulp-uglify'),
+      concat       = require('gulp-concat'),
+      rename       = require('gulp-rename');
+
+// --------------------------------------------------------------------
+// Task: sass
+// compile scss-file into css-file
+// --------------------------------------------------------------------
+
+gulp.task('sass', function () {
+    return gulp.src('./public/scss/app.scss')
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('public/css'));
+});
+
+// --------------------------------------------------------------------
+// Task: mincss
+// minify the the generated css-file
+// --------------------------------------------------------------------
+
+gulp.task('mincss', ['sass'], function () {
+    return gulp.src('public/css/app.css')
+        .pipe(csso())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('public/css'));
+});
+
+// --------------------------------------------------------------------
+// Task: concat
+// take all js-files and generate a single js-file out of them
+// --------------------------------------------------------------------
+
+// snippet scripts src: public/js/scripts/*
+// generated single script src: public/js/logic.js
+// minified single script src: public/js/logic.min.js
+
+// order of files -> order in final js
+var jsFiles = [
+    'public/js/scripts/script.js'
+];
+
+gulp.task('concat', function () {
+    return gulp.src(jsFiles)
+        .pipe(concat('logic.js'))
+        .pipe(gulp.dest('public/js'));
+});
+
+// --------------------------------------------------------------------
+// Task: minjs
+// minify and uglify the js-file generated with concat
+// --------------------------------------------------------------------
+
+gulp.task('minjs', ['concat'], function () {
+    return gulp.src('public/js/logic.js')
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('public/js'));
+});
+
+// --------------------------------------------------------------------
+// watch task
+// watch for file changes and override generated files
+// --------------------------------------------------------------------
+
+gulp.task('watch', function () {
+  gulp.watch(['./public/scss/*.scss','./public/scss/**/*.scss'], ['sass', 'mincss']);
+  gulp.watch(['./public/js/scripts/*.js'], ['concat', 'minjs']);
+});
+
+// --------------------------------------------------------------------
+// default Task
+// building scss and js assets to single minifed min.css and min.js files
+// --------------------------------------------------------------------
+
+gulp.task('default', ['sass', 'mincss', 'concat', 'minjs']);
